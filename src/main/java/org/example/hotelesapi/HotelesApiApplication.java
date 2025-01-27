9b655251c418445140573db0298eb28a5a09513e
angel.chiveg <angel.chiveg@educa.jcyl.es>
2025-01-27 14:18:13 +0100

    Añadir la configuración de seguridad, se puede ver la doc

--- src/main/java/org/example/hotelesapi/HotelesApiApplication.java
+++ src/main/java/org/example/hotelesapi/HotelesApiApplication.java
@@ -1,7 +1,14 @@
 package org.example.hotelesapi;
 
+import org.example.hotelesapi.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class HotelesApiApplication {
@@ -10,4 +17,21 @@ public class HotelesApiApplication {
         SpringApplication.run(HotelesApiApplication.class, args);
     }
 
+    @Configuration
+    public static class SecurityConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers(HttpMethod.POST, "/user").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/hotel/{localidad}/{categoria}").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/habitacion/{hotel}/{capacidad}/{precioMin}/{precioMax}").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/v3/**").permitAll()
+                            .anyRequest().authenticated());
+
+            return http.build();
+        }
+    }
 }
